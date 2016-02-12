d5a67a4bf86a3448afd3cee7f9c4188df52f7446
nmondon <nmondon@lefigaro.fr>
2016-02-12 18:38:04 +0100

    issue #2 : tests and coverage

--- test/kohonenTest.js
+++ test/kohonenTest.js
@@ -1,4 +1,25 @@
 'use strict';
 
+import chai, { assert, expect } from 'chai'
+import spies from 'chai-spies'
+import Kohonen from '../src/Kohonen'
+
+chai.use(spies);
+
+describe('Kohonen', ()=>{
+
+    describe('static range', ()=>{
+
+        it('should return an array', ()=>{
+            assert.isArray(Kohonen.range());
+        });
+
+        it('should return an array with the specified length', () => {
+            assert.lengthOf(Kohonen.range(), 0);
+            assert.lengthOf(Kohonen.range(10), 10);
+        });
 
 
+    });
+
+});
