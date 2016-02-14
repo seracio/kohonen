ec383e9ab453df37717560229efb2cf73c8f17af
hllwd <nicolasmondon@gmail.com>
2016-02-14 19:49:44 +0100

    issue #2 : static methods test

--- src/kohonen.js
+++ src/kohonen.js
@@ -1,12 +1,19 @@
 'use strict';
 
+import d3 from 'd3';
+
 class Kohonen {
 
     constructor({ size, neurons }) {
-        this.dimSize = dimSize;
+        this.size = size;
+
+        // On each neuron, generate a random vector v
+        // of <size> dimension
         this.neurons = neurons.map(n => Object.assign({}, n, {
             v: Kohonen.generateRandomVector({size})
         }));
+
+        // Initialize step
         this.step = 0;
     }
 
@@ -20,12 +27,24 @@ Kohonen.scaleStepLearningCoef = () => {
 
 };
 
-Kohonen.scaleStepNeighborhood = () => {
-
-};
+// Decrease neighborhood with time
+Kohonen.scaleStepNeighborhood = step => d3.scale.linear()
+    .clamp(true)
+    .domain([0, 10000])
+    .range([1, .3])(step);
 
 // http://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
-Kohonen.neighborhood = () => {
+Kohonen.neighborhood = ({ bmu, n, step }) => {
+
+    const a = 1;
+    const sigmaX = 1;
+    const sigmaY = 1;
+
+    return a
+        * Math.exp(
+            -(Math.pow(n.x - bmu.x, 2) / 2 * Math.pow(sigmaX, 2) + Math.pow(n.y - bmu.y, 2) / 2 * Math.pow(sigmaY, 2))
+        )
+        * Kohonen.scaleStepNeighborhood(step);
 
 };
 
@@ -35,7 +54,7 @@ Kohonen.findBestMatchingUnit = v => {
 
 // For a given size, return an array of `size` with random values
 // between [0,1]
-Kohonen.generateRandomVector = ({size}) => Kohonen
+Kohonen.generateRandomVector = ({size = 0} = {}) => Kohonen
     .range(size)
     .map(i => Math.random());
 
@@ -43,3 +62,5 @@ Kohonen.generateRandomVector = ({size}) => Kohonen
 // not that trivial,
 // see http://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n
 Kohonen.range = length => Array.apply(null, {length});
+
+export default Kohonen;
--- test/kohonenTest.js
+++ test/kohonenTest.js
@@ -6,11 +6,15 @@ import Kohonen from '../src/Kohonen'
 
 chai.use(spies);
 
-describe('Kohonen', ()=>{
+describe('Kohonen', ()=> {
 
-    describe('static range', ()=>{
+    describe('static range', ()=> {
 
-        it('should return an array', ()=>{
+        it('should be defined as a function', ()=> {
+            assert.isFunction(Kohonen.range);
+        });
+
+        it('should return an array', ()=> {
             assert.isArray(Kohonen.range());
         });
 
@@ -19,6 +23,64 @@ describe('Kohonen', ()=>{
             assert.lengthOf(Kohonen.range(10), 10);
         });
 
+    });
+
+    describe('static scaleStepNeighborhood', () => {
+
+        it('should be defined as a function', ()=> {
+            assert.isFunction(Kohonen.scaleStepNeighborhood);
+        });
+
+        it('should return a number', () => {
+            assert.isNumber(Kohonen.scaleStepNeighborhood(0));
+            assert.isNumber(Kohonen.scaleStepNeighborhood(10000));
+            assert.isNumber(Kohonen.scaleStepNeighborhood(100000));
+        });
+
+        it('shoud return a value between 1 and .3 and decrease with time', () => {
+            assert.equal(Kohonen.scaleStepNeighborhood(0), 1);
+            assert.equal(Kohonen.scaleStepNeighborhood(10000), .3);
+            assert.equal(Kohonen.scaleStepNeighborhood(100000), .3);
+        });
+
+    });
+
+    describe('static generateRandomVector', () => {
+
+        it('should be defined as a function', () => {
+            assert.isFunction(Kohonen.generateRandomVector);
+        });
+
+        it('should return an array', () => {
+            assert.isArray(Kohonen.generateRandomVector());
+            assert.isArray(Kohonen.generateRandomVector(10));
+        });
+
+        it('should return an empty array if parameter is not defined', () => {
+            assert.lengthOf(Kohonen.generateRandomVector(), 0);
+        });
+
+        it('should return an array with a lengthOf size parameter', () => {
+            assert.lengthOf(Kohonen.generateRandomVector({size: 0}), 0);
+            assert.lengthOf(Kohonen.generateRandomVector({size: 10}), 10);
+        });
+
+        it('should return an array with values between 0 and 1', () => {
+            Kohonen.generateRandomVector({size: 10})
+                .map( val => assert.isTrue( val >= 0 && val <= 1 ));
+        });
+
+    });
+
+    describe('static neighborhood', () => {
+
+        it('should be defined as a function', () => {
+            assert.isFunction(Kohonen.neighborhood);
+        });
+
+        it('should throw an Error if parameters are not well defined', () => {
+
+        });
 
     });
 
