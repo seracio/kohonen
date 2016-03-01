c8a1a54abbb171b8acaed258dd86290f6d6929ab
nmondon <nmondon@lefigaro.fr>
2016-03-01 18:44:21 +0100

    issue #2 : implement and test grid with normalized hexagons

--- src/vector.js
+++ src/vector.js
@@ -3,7 +3,7 @@
 import _ from 'lodash/fp';
 
 // euclidian distance of 2 vectors
-export const dist = (v1,v2) => Math.sqrt(v1.reduce((seed, cur, ind) => seed + Math.pow(v2[ind] - cur, 2), 0));
+export const dist = (v1, v2) => Math.sqrt(v1.reduce((seed, cur, ind) => seed + Math.pow(v2[ind] - cur, 2), 0));
 
 // scalar mult of a vector
 export const mult = (v, coef) => v.map(val => val * coef);
@@ -16,4 +16,4 @@ export const add = (v1, v2) => v1.map((val, i) => v2[i] + val);
 
 // For a given size, return an array of `size` with random values
 // between [0,1]
-export const random = size => _.range(0,size).map(i => Math.random());
+export const random = size => _.range(0, size).map(i => Math.random());
---
+++ test/hexagonTest.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import chai, { assert, expect } from 'chai';
+import spies from 'chai-spies';
+import { generateGrid } from '../src/hexagon';
+import { dist } from '../src/vector';
+import { round } from '../src/math';
+
+chai.use(spies);
+
+describe('hexagon', ()=>{
+
+    it('should return an array', () => {
+        assert.isArray(generateGrid(10,10));
+        assert.isArray(generateGrid(10,1));
+        assert.isArray(generateGrid(10,5));
+    });
+
+    it('should return an array of sizeX * sizeY length', () => {
+        assert.lengthOf(generateGrid(10,10), 100);
+        assert.lengthOf(generateGrid(5,10), 50);
+        assert.lengthOf(generateGrid(5,1), 5);
+    });
+
+    it('should return an array of {x,y} elements', () => {
+        const grid = generateGrid(10,10);
+        grid.forEach( g => {
+            assert.isObject(g);
+            assert.property(g, 'x');
+            assert.property(g, 'y');
+        });
+    });
+
+    it('should have normalized euclidian distances', ()=> {
+        const grid = generateGrid(10,10).map( ({x,y}) => [x,y] );
+        assert.equal(1, dist(grid[0], grid[1]));
+        assert.equal(1, Math.round(dist(grid[0], grid[10])), 2);
+        assert.equal(1, Math.round(dist(grid[10], grid[11])), 2);
+        assert.equal(1, Math.round(dist(grid[10], grid[20])), 2);
+    });
+
+});
