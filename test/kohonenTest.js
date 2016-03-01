5ced6cc1efad76292cb52f580cd29b040a22cd4f
hllwd <nicolasmondon@gmail.com>
2016-03-01 20:16:25 +0100

    issue #2 : remove static methods

--- test/kohonenTest.js
+++ test/kohonenTest.js
@@ -9,36 +9,4 @@ chai.use(spies);
 describe('Kohonen', ()=> {
 
 
-    describe('static scaleStepNeighborhood', () => {
-
-        it('should be defined as a function', ()=> {
-            assert.isFunction(Kohonen.scaleStepNeighborhood);
-        });
-
-        it('should return a number', () => {
-            assert.isNumber(Kohonen.scaleStepNeighborhood(0));
-            assert.isNumber(Kohonen.scaleStepNeighborhood(10000));
-            assert.isNumber(Kohonen.scaleStepNeighborhood(100000));
-        });
-
-        it('shoud return a value between 1 and .3 and decrease with time', () => {
-            assert.equal(Kohonen.scaleStepNeighborhood(0), 1);
-            assert.equal(Kohonen.scaleStepNeighborhood(10000), .3);
-            assert.equal(Kohonen.scaleStepNeighborhood(100000), .3);
-        });
-
-    });
-
-    describe('static neighborhood', () => {
-
-        it('should be defined as a function', () => {
-            assert.isFunction(Kohonen.neighborhood);
-        });
-
-        it('should throw an Error if parameters are not well defined', () => {
-
-        });
-
-    });
-
 });
