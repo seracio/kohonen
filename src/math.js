d9108eb8a12c6d6ee3e3d51468087ba7f6064e54
nmondon <nmondon@lefigaro.fr>
2016-03-01 16:08:53 +0100

    issue #2 : add a math module to normalize inputs

--- src/math.js
+++ src/math.js
@@ -1,7 +1,10 @@
 'use strict';
 
 // return a gaussian normalization for a given val, a mean and a standard deviation
+//
 // https://visualstudiomagazine.com/articles/2014/01/01/how-to-standardize-data-for-neural-networks.aspx
+//
+// https://en.wikipedia.org/wiki/Normal_distribution
 export const gaussianNormalization = (val, mean, standardDeviation) => (val - mean) / standardDeviation;
 
 export const mean = inputs => inputs.reduce((sum, cur) => sum + cur, 0) / inputs.length;
