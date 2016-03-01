087f39e8420cccfdb22ff2f5bd6fc44b30df776c
hllwd <nicolasmondon@gmail.com>
2016-03-01 20:20:52 +0100

    issue #2 : remove unused immutability

--- src/kohonen.js
+++ src/kohonen.js
@@ -15,6 +15,10 @@ class Kohonen {
     // The constructor needs two params :
     // * size : the dimension size of the vectors
     // * neurons : an already built neurons grid as an array
+    // * maxStep : the max step that will be clamped in scaleStepLearningCoef and
+    //             scaleStepNeighborhood
+    // * minLearningCoef
+    // * minNeighborhood
     //
     // each neuron should provide :
     // * an x
@@ -25,8 +29,7 @@ class Kohonen {
     //
     // You also should normalized your neighborhood in such a way that 2 neighbors
     // got an euclidian distance of 1 between each other.
-    constructor({ size, neurons }) {
-        this.size = size;
+    constructor({ size, neurons, maxStep = 10000, minLearningCoef = .3, minNeighborhood = .3 }) {
 
         // On each neuron, generate a random vector v
         // of <size> dimension
@@ -36,67 +39,63 @@ class Kohonen {
 
         // Initialize step
         this.step = 0;
+
+        // generate scaleStepLearningCoef,
+        // as the learning coef decreases with time
+        this.scaleStepLearningCoef = d3.scale.linear()
+            .clamp(true)
+            .domain([0, maxStep])
+            .range([1, minLearningCoef]);
+
+        // decrease neighborhood with time
+        this.scaleStepNeighborhood = d3.scale.linear()
+            .clamp(true)
+            .domain([0, maxStep])
+            .range([1, minNeighborhood]);
+
     }
 
     learn(v) {
         // find bmu
         const bmu = this.findBestMatchingUnit(v);
         // compute current learning coef
-        const currentLearningCoef = Kohonen.scaleStepLearningCoef(this.step);
+        const currentLearningCoef = this.scaleStepLearningCoef(this.step);
 
         this.neurons.forEach(n => {
             // compute neighborhood
-            const currentNeighborhood = Kohonen.neighborhood({bmu, n, step: this.step});
+            const currentNeighborhood = this.neighborhood({bmu, n});
             // compute delta for the current neuron
             const delta = mult(
                 diff(n.v, v),
                 currentNeighborhood * currentLearningCoef
             );
             // update current vector
-            n.v = [...add(n.v, delta)];
+            n.v = add(n.v, delta);
         });
         this.step += 1;
     }
 
     // Find closer neuron
     findBestMatchingUnit(v){
-        return _.sortBy(n => dist(v, n.v))(this.neurons)[0];
+        return _.flow(_.sortBy(n => dist(v, n.v)), _.first)(this.neurons);
+    }
+
+    // http://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
+    //
+    // http://mathworld.wolfram.com/GaussianFunction.html
+    //
+    // neighborhood function made with a gaussian
+    neighborhood({ bmu, n}){
+        const a = 1;
+        const sigmaX = 1;
+        const sigmaY = 1;
+        return a
+            * Math.exp(
+                -(Math.pow(n.x - bmu.x, 2) / 2 * Math.pow(sigmaX, 2) + Math.pow(n.y - bmu.y, 2) / 2 * Math.pow(sigmaY, 2))
+            )
+            * this.scaleStepNeighborhood(this.step);
     }
 
 }
-
-// The learning coef decreases with time
-// TODO should be parametrizable ?
-// TODO And remove from static
-Kohonen.scaleStepLearningCoef = step => d3.scale.linear()
-    .clamp(true)
-    .domain([0, 10000])
-    .range([1, .3])(step);
-
-// Decrease neighborhood with time
-// TODO should be parametrizable ?
-// TODO And remove from static
-Kohonen.scaleStepNeighborhood = step => d3.scale.linear()
-    .clamp(true)
-    .domain([0, 10000])
-    .range([1, .3])(step);
-
-// http://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
-//
-// http://mathworld.wolfram.com/GaussianFunction.html
-//
-// neighborhood function made with a gaussian
-// TODO a, sigmaX, sigmaY should be parametrizable
-Kohonen.neighborhood = ({ bmu, n, step }) => {
-    const a = 1;
-    const sigmaX = 1;
-    const sigmaY = 1;
-    return a
-        * Math.exp(
-            -(Math.pow(n.x - bmu.x, 2) / 2 * Math.pow(sigmaX, 2) + Math.pow(n.y - bmu.y, 2) / 2 * Math.pow(sigmaY, 2))
-        )
-        * Kohonen.scaleStepNeighborhood(step);
-};
-
 
 export default Kohonen;
