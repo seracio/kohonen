4c1c9758d904c88efc0427198444f145f264fb4a
nmondon <nmondon@lefigaro.fr>
2016-03-02 14:13:00 +0100

    issue #5 : add dist script

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import * as hexagonHelper from './hexagon';
+import Kohonen from './Kohonen';
+import * as mathHelper from './math';
+import * as vectorHelper from './vector';
+
+export default Kohonen;
+export { hexagonHelper, mathHelper, vectorHelper };
---
+++ test/distTest.js
@@ -0,0 +1,71 @@
+'use strict';
+
+import chai, { assert, expect } from 'chai';
+import spies from 'chai-spies';
+
+import Kohonen, {mathHelper, hexagonHelper, vectorHelper} from '../dist/index'
+
+chai.use(spies);
+
+describe('dist', ()=>{
+
+    describe('Kohonen', () => {
+
+        it('should be defined', ()=>{
+            assert.isDefined(Kohonen);
+        });
+
+        it('should be a class', ()=>{
+            assert.instanceOf(new Kohonen({size: 3, neurons: [{pos: [0,0]}]}), Kohonen);
+        });
+
+    });
+
+    describe('mathHelper', () => {
+
+        it('should be defined', ()=>{
+            assert.isDefined(mathHelper);
+        });
+
+        it('should be an object', ()=>{
+            assert.isObject(mathHelper);
+            assert.property(mathHelper, 'gaussianNormalization');
+            assert.property(mathHelper, 'mean');
+            assert.property(mathHelper, 'variance');
+            assert.property(mathHelper, 'standardDeviation');
+            assert.property(mathHelper, 'round');
+        });
+
+    });
+
+    describe('hexagonHelper', () => {
+
+        it('should be defined', ()=>{
+            assert.isDefined(hexagonHelper);
+        });
+
+        it('should be an object', ()=>{
+            assert.isObject(hexagonHelper);
+            assert.property(hexagonHelper, 'generateGrid');
+        });
+
+    });
+
+    describe('vectorHelper', () => {
+
+        it('should be defined', ()=>{
+            assert.isDefined(vectorHelper);
+        });
+
+        it('should be an object', ()=>{
+            assert.isObject(vectorHelper);
+            assert.property(vectorHelper, 'dist');
+            assert.property(vectorHelper, 'mult');
+            assert.property(vectorHelper, 'diff');
+            assert.property(vectorHelper, 'add');
+            assert.property(vectorHelper, 'random');
+        });
+
+    });
+
+});
