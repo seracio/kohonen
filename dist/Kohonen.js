96a6c00f799fe2ed48b04c306671a954fe591582
nmondon <nmondon@lefigaro.fr>
2016-12-01 13:26:12 +0100

    build

--- dist/Kohonen.js
+++ dist/Kohonen.js
@@ -1,12 +1,12 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-// lodash/fp random has a fixed arity of 2, without the last (and useful) param
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _d3Scale = require('d3-scale');
 
@@ -22,13 +22,15 @@ var _mlPca2 = _interopRequireDefault(_mlPca);
 
 var _vector = require('./vector');
 
-var _random = require('lodash/random');
-
-var _random2 = _interopRequireDefault(_random);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// lodash/fp random has a fixed arity of 2, without the last (and useful) param
+var random = _fp2.default.random.convert({ fixed: false });
+
+// lodash/fp map has an iteratee with a single arg
+var mapWithIndex = _fp2.default.map.convert({ cap: false });
 
 // A basic implementation of Kohonen map
 
@@ -38,221 +40,236 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var Kohonen = function () {
 
-    // The constructor needs two params :
-    // * neurons : an already built neurons grid as an array
-    // * data : data set to consider
-    // * maxStep : the max step that will be clamped in scaleStepLearningCoef and
-    //             scaleStepNeighborhood
-    // * maxLearningCoef
-    // * minLearningCoef
-    // * maxNeighborhood
-    // * minNeighborhood
-    //
-    // each neuron should provide a 2D vector pos,
-    // which refer to the grid position
-    //
-    // You should use an hexagon grid as it is the easier case
-    // to deal with neighborhood.
-    //
-    // You also should normalized your neighborhood in such a way that 2 neighbors
-    // got an euclidian distance of 1 between each other.
+  // The constructor needs two params :
+  // * neurons : an already built neurons grid as an array
+  // * data : data set to consider
+  // * maxStep : the max step that will be clamped in scaleStepLearningCoef and
+  //             scaleStepNeighborhood
+  // * maxLearningCoef
+  // * minLearningCoef
+  // * maxNeighborhood
+  // * minNeighborhood
+  //
+  // each neuron should provide a 2D vector pos,
+  // which refer to the grid position
+  //
+  // You should use an hexagon grid as it is the easier case
+  // to deal with neighborhood.
+  //
+  // You also should normalized your neighborhood in such a way that 2 neighbors
+  // got an euclidian distance of 1 between each other.
+  function Kohonen(_ref) {
+    var neurons = _ref.neurons,
+        data = _ref.data,
+        _ref$maxStep = _ref.maxStep,
+        maxStep = _ref$maxStep === undefined ? 10000 : _ref$maxStep,
+        _ref$minLearningCoef = _ref.minLearningCoef,
+        minLearningCoef = _ref$minLearningCoef === undefined ? .1 : _ref$minLearningCoef,
+        _ref$maxLearningCoef = _ref.maxLearningCoef,
+        maxLearningCoef = _ref$maxLearningCoef === undefined ? .4 : _ref$maxLearningCoef,
+        _ref$minNeighborhood = _ref.minNeighborhood,
+        minNeighborhood = _ref$minNeighborhood === undefined ? .3 : _ref$minNeighborhood,
+        _ref$maxNeighborhood = _ref.maxNeighborhood,
+        maxNeighborhood = _ref$maxNeighborhood === undefined ? 1 : _ref$maxNeighborhood;
 
-    function Kohonen(_ref) {
-        var neurons = _ref.neurons;
-        var data = _ref.data;
-        var _ref$maxStep = _ref.maxStep;
-        var maxStep = _ref$maxStep === undefined ? 10000 : _ref$maxStep;
-        var _ref$maxLearningCoef = _ref.maxLearningCoef;
-        var maxLearningCoef = _ref$maxLearningCoef === undefined ? 1 : _ref$maxLearningCoef;
-        var _ref$minLearningCoef = _ref.minLearningCoef;
-        var minLearningCoef = _ref$minLearningCoef === undefined ? .3 : _ref$minLearningCoef;
-        var _ref$maxNeighborhood = _ref.maxNeighborhood;
-        var maxNeighborhood = _ref$maxNeighborhood === undefined ? 1 : _ref$maxNeighborhood;
-        var _ref$minNeighborhood = _ref.minNeighborhood;
-        var minNeighborhood = _ref$minNeighborhood === undefined ? .3 : _ref$minNeighborhood;
+    _classCallCheck(this, Kohonen);
 
-        _classCallCheck(this, Kohonen);
-
-        this.size = data[0].length;
-        this.step = 0;
-        this.maxStep = maxStep;
-
-        // generate scaleStepLearningCoef,
-        // as the learning coef decreases with time
-        this.scaleStepLearningCoef = (0, _d3Scale.scaleLinear)().clamp(true).domain([0, maxStep]).range([maxLearningCoef, minLearningCoef]);
-
-        // decrease neighborhood with time
-        this.scaleStepNeighborhood = (0, _d3Scale.scaleLinear)().clamp(true).domain([0, maxStep]).range([maxNeighborhood, minNeighborhood]);
-
-        // retrive min and max for each feature
-        var unnormalizedExtents = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.extent))(data);
-
-        // build scales for data normalization
-        var scales = unnormalizedExtents.map(function (extent) {
-            return (0, _d3Scale.scaleLinear)().domain(extent).range([0, 1]);
-        });
-
-        // build normalized data
-        this.data = this.normalize(data, scales);
-
-        // then we store means and deviations for normalized datas
-        this.means = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.mean))(this.data);
-        this.deviations = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.deviation))(this.data);
-
-        // On each neuron, generate a random vector v
-        // of <size> dimension
-        var randomInitialVectors = this.generateInitialVectors(neurons.length);
-        this.neurons = neurons.map(function (n, i) {
-            return Object.assign({}, n, {
-                v: randomInitialVectors[i]
-            });
-        });
+    // data vectors should have at least one dimension
+    if (!data[0].length) {
+      throw new Error('Kohonen constructor: data vectors should have at least one dimension');
     }
 
-    _createClass(Kohonen, [{
-        key: 'normalize',
-        value: function normalize(data, scales) {
-            return data.map(function (v) {
-                return v.map(function (s, i) {
-                    return scales[i](s);
-                });
-            });
-        }
+    // all vectors should have the same size
+    // all vectors values should be number
+    for (var ind in data) {
+      if (data[ind].length !== data[0].length) {
+        throw new Error('Kohonen constructor: all vectors should have the same size');
+      }
+      var allNum = _fp2.default.reduce(function (seed, current) {
+        return seed && !isNaN(current) && isFinite(current);
+      }, true, data[ind]);
+      if (!allNum) {
+        throw new Error('Kohonen constructor: all vectors should number values');
+      }
+    }
 
-        // learn and return corresponding neurons for the dataset
+    this.size = data[0].length;
+    this.numNeurons = neurons.length;
+    this.step = 0;
+    this.maxStep = maxStep;
 
-    }, {
-        key: 'training',
-        value: function training() {
-            var log = arguments.length <= 0 || arguments[0] === undefined ? function () {} : arguments[0];
+    // generate scaleStepLearningCoef,
+    // as the learning coef decreases with time
+    this.scaleStepLearningCoef = (0, _d3Scale.scaleLinear)().clamp(true).domain([0, maxStep]).range([maxLearningCoef, minLearningCoef]);
 
-            for (var i = 0; i < this.maxStep; i++) {
-                // generate a random vector
-                this.learn(this.generateLearningVector());
-                log(this.neurons, this.step);
-            }
-        }
-    }, {
-        key: 'mapping',
-        value: function mapping() {
-            return _fp2.default.flow(_fp2.default.map(this.findBestMatchingUnit.bind(this)), _fp2.default.map(function (n) {
-                return n.pos;
-            }))(this.data);
-        }
+    // decrease neighborhood with time
+    this.scaleStepNeighborhood = (0, _d3Scale.scaleLinear)().clamp(true).domain([0, maxStep]).range([maxNeighborhood, minNeighborhood]);
 
-        // The U-Matrix value of a particular node
-        // is the average distance between the node's weight vector and that of its closest neighbors.
+    // retrive min and max for each feature
+    var unnormalizedExtents = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.extent))(data);
 
-    }, {
-        key: 'umatrix',
-        value: function umatrix() {
-            var _this = this;
+    // build scales for data normalization
+    var scales = unnormalizedExtents.map(function (extent) {
+      return (0, _d3Scale.scaleLinear)().domain(extent).range([0, 1]);
+    });
 
-            var roundToTwo = function roundToTwo(num) {
-                return +(Math.round(num + "e+2") + "e-2");
-            };
-            var findNeighors = function findNeighors(cn) {
-                return _fp2.default.filter(function (n) {
-                    return roundToTwo((0, _vector.dist)(n.pos, cn.pos)) === 1;
-                }, _this.neurons);
-            };
-            return _fp2.default.map(function (n) {
-                return (0, _d3Array.mean)(findNeighors(n).map(function (nb) {
-                    return (0, _vector.dist)(nb.v, n.v);
-                }));
-            }, this.neurons);
-        }
+    // build normalized data
+    this.data = this.normalize(data, scales);
 
-        // pick a random vector among data
+    // then we store means and deviations for normalized datas
+    this.means = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.mean))(this.data);
 
-    }, {
-        key: 'generateLearningVector',
-        value: function generateLearningVector() {
-            return this.data[_fp2.default.random(0, this.data.length - 1)];
-        }
-    }, {
-        key: 'generateInitialVectors',
-        value: function generateInitialVectors(dataSize) {
-            var _this2 = this;
+    this.deviations = _fp2.default.flow(_fp2.default.unzip, _fp2.default.map(_d3Array.deviation))(this.data);
 
-            // principal component analysis
-            // standardize to false as we already standardize ours
-            var pca = new _mlPca2.default(this.data, {
-                standardize: false
-            });
-            // centered covariance eigenvectors
-            var eigenvectors = pca.getEigenvectors();
-            // eigenvalues
-            var eigenvalues = pca.getEigenvalues();
-            // scale eigenvectors to the square root of eigenvalues
-            // we'll only keep the 2 largest eigenvectors
-            var scaledEigenvectors = _fp2.default.take(2, eigenvectors.map(function (v, i) {
-                return (0, _vector.mult)(v, Math.sqrt(eigenvalues[i]));
-            }));
-            // function to generate random vectors into eigenvectors space
-            var generateRandomVecWithinEigenvectorsSpace = function generateRandomVecWithinEigenvectorsSpace() {
-                return (0, _vector.add)((0, _vector.mult)(scaledEigenvectors[0], (0, _random2.default)(-1, 1, true)), (0, _vector.mult)(scaledEigenvectors[1], (0, _random2.default)(-1, 1, true)));
-            };
+    // On each neuron, generate a random vector v
+    // of <size> dimension
+    var randomInitialVectors = this.generateInitialVectors();
+    this.neurons = mapWithIndex(function (neuron, i) {
+      return _extends({}, neuron, {
+        v: randomInitialVectors[i]
+      });
+    }, neurons);
+  }
 
-            // we generate all random vectors and uncentered them by adding means vector
-            return _fp2.default.map(function () {
-                return (0, _vector.add)(generateRandomVecWithinEigenvectorsSpace(), _this2.means);
-            }, _fp2.default.range(0, dataSize));
-        }
-    }, {
-        key: 'learn',
-        value: function learn(v) {
-            var _this3 = this;
+  _createClass(Kohonen, [{
+    key: 'normalize',
+    value: function normalize(data, scales) {
+      return data.map(function (v) {
+        return v.map(function (s, i) {
+          return scales[i](s);
+        });
+      });
+    }
 
-            // find bmu
-            var bmu = this.findBestMatchingUnit(v);
-            // compute current learning coef
-            var currentLearningCoef = this.scaleStepLearningCoef(this.step);
+    // learn and return corresponding neurons for the dataset
 
-            this.neurons.forEach(function (n) {
-                // compute neighborhood
-                var currentNeighborhood = _this3.neighborhood({ bmu: bmu, n: n });
+  }, {
+    key: 'training',
+    value: function training() {
+      var log = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function () {};
 
-                // compute delta for the current neuron
-                var delta = (0, _vector.mult)((0, _vector.diff)(n.v, v), currentNeighborhood * currentLearningCoef);
+      for (var i = 0; i < this.maxStep; i++) {
+        // generate a random vector
+        this.learn(this.generateLearningVector());
+        log(this.neurons, this.step);
+      }
+    }
+  }, {
+    key: 'mapping',
+    value: function mapping() {
+      return _fp2.default.map(_fp2.default.flow(this.findBestMatchingUnit.bind(this), _fp2.default.get('pos')), this.data);
+    }
 
-                // update current vector
-                n.v = (0, _vector.add)(n.v, delta);
-            });
-            this.step += 1;
-        }
+    // The U-Matrix value of a particular node
+    // is the average distance between the node's weight vector and that of its closest neighbors.
 
-        // Find closer neuron
+  }, {
+    key: 'umatrix',
+    value: function umatrix() {
+      var _this = this;
 
-    }, {
-        key: 'findBestMatchingUnit',
-        value: function findBestMatchingUnit(v) {
-            return _fp2.default.flow(_fp2.default.sortBy(function (n) {
-                return (0, _vector.dist)(v, n.v);
-            }), _fp2.default.first)(this.neurons);
-        }
+      var roundToTwo = function roundToTwo(num) {
+        return +(Math.round(num + "e+2") + "e-2");
+      };
+      var findNeighors = function findNeighors(cn) {
+        return _fp2.default.filter(function (n) {
+          return roundToTwo((0, _vector.dist)(n.pos, cn.pos)) === 1;
+        }, _this.neurons);
+      };
+      return _fp2.default.map(function (n) {
+        return (0, _d3Array.mean)(findNeighors(n).map(function (nb) {
+          return (0, _vector.dist)(nb.v, n.v);
+        }));
+      }, this.neurons);
+    }
 
-        // http://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
-        //
-        // http://mathworld.wolfram.com/GaussianFunction.html
-        //
-        // neighborhood function made with a gaussian
+    // pick a random vector among data
 
-    }, {
-        key: 'neighborhood',
-        value: function neighborhood(_ref2) {
-            var bmu = _ref2.bmu;
-            var n = _ref2.n;
+  }, {
+    key: 'generateLearningVector',
+    value: function generateLearningVector() {
+      return this.data[_fp2.default.random(0, this.data.length - 1)];
+    }
+  }, {
+    key: 'generateInitialVectors',
+    value: function generateInitialVectors() {
+      var _this2 = this;
 
-            var a = 1;
-            var sigmaX = 1;
-            var sigmaY = 1;
+      // principal component analysis
+      // standardize to false as we already standardize ours
+      //
+      var pca = new _mlPca2.default(this.data, {
+        center: true,
+        scale: false
+      });
 
-            return a * Math.exp(-(Math.pow(n.pos[0] - bmu.pos[0], 2) / 2 * Math.pow(sigmaX, 2) + Math.pow(n.pos[1] - bmu.pos[1], 2) / 2 * Math.pow(sigmaY, 2))) * this.scaleStepNeighborhood(this.step);
-        }
-    }]);
+      // we'll only keep the 2 largest eigenvectors
+      var transposedEV = _fp2.default.take(2, pca.getLoadings());
 
-    return Kohonen;
+      // function to generate random vectors into eigenvectors space
+      var generateRandomVecWithinEigenvectorsSpace = function generateRandomVecWithinEigenvectorsSpace() {
+        return (0, _vector.add)((0, _vector.mult)(transposedEV[0], random(-.5, .5, true)), (0, _vector.mult)(transposedEV[1], random(-.5, .5, true)));
+      };
+
+      // we generate all random vectors and uncentered them by adding means vector
+      return _fp2.default.map(function () {
+        return (0, _vector.add)(generateRandomVecWithinEigenvectorsSpace(), _this2.means);
+      }, _fp2.default.range(0, this.numNeurons));
+    }
+  }, {
+    key: 'learn',
+    value: function learn(v) {
+      var _this3 = this;
+
+      // find bmu
+      var bmu = this.findBestMatchingUnit(v);
+      // compute current learning coef
+      var currentLearningCoef = this.scaleStepLearningCoef(this.step);
+
+      this.neurons.forEach(function (n) {
+        // compute neighborhood
+        var currentNeighborhood = _this3.neighborhood({ bmu: bmu, n: n });
+
+        // compute delta for the current neuron
+        var delta = (0, _vector.mult)((0, _vector.diff)(n.v, v), currentNeighborhood * currentLearningCoef);
+
+        // update current vector
+        n.v = (0, _vector.add)(n.v, delta);
+      });
+      this.step += 1;
+    }
+
+    // Find closer neuron
+
+  }, {
+    key: 'findBestMatchingUnit',
+    value: function findBestMatchingUnit(v) {
+      return _fp2.default.flow(_fp2.default.orderBy(function (n) {
+        return (0, _vector.dist)(v, n.v);
+      }, 'asc'), _fp2.default.first)(this.neurons);
+    }
+
+    // http://en.wikipedia.org/wiki/Gaussian_function#Two-dimensional_Gaussian_function
+    //
+    // http://mathworld.wolfram.com/GaussianFunction.html
+    //
+    // neighborhood function made with a gaussian
+
+  }, {
+    key: 'neighborhood',
+    value: function neighborhood(_ref2) {
+      var bmu = _ref2.bmu,
+          n = _ref2.n;
+
+      var a = 1;
+      var sigmaX = 1;
+      var sigmaY = 1;
+
+      return a * Math.exp(-(Math.pow(n.pos[0] - bmu.pos[0], 2) / 2 * Math.pow(sigmaX, 2) + Math.pow(n.pos[1] - bmu.pos[1], 2) / 2 * Math.pow(sigmaY, 2))) * this.scaleStepNeighborhood(this.step);
+    }
+  }]);
+
+  return Kohonen;
 }();
 
 exports.default = Kohonen;
--- dist/vector.js
+++ dist/vector.js
@@ -1,9 +1,9 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
-exports.norm = exports.random = exports.add = exports.diff = exports.mult = exports.dist = undefined;
+exports.random = exports.add = exports.diff = exports.mult = exports.dist = undefined;
 
 var _fp = require('lodash/fp');
 
@@ -15,30 +15,34 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // euclidian distance of 2 vectors
 var dist = exports.dist = function dist(v1, v2) {
-    return Math.sqrt(v1.reduce(function (seed, cur, ind) {
-        return seed + Math.pow(v2[ind] - cur, 2);
-    }, 0));
+  var d = Math.sqrt(v1.reduce(function (seed, cur, ind) {
+    return seed + Math.pow(v2[ind] - cur, 2);
+  }, 0));
+  if (isNaN(d) || !isFinite(d)) {
+    throw new Error('vector.dist : not a number');
+  }
+  return d;
 };
 
 // scalar mult of a vector
 var mult = exports.mult = function mult(v, coef) {
-    return v.map(function (val) {
-        return val * coef;
-    });
+  return v.map(function (val) {
+    return val * coef;
+  });
 };
 
 // scalar diff of a vector
 var diff = exports.diff = function diff(v1, v2) {
-    return v1.map(function (val, i) {
-        return v2[i] - val;
-    });
+  return v1.map(function (val, i) {
+    return v2[i] - val;
+  });
 };
 
 // scalar addition of a vector
 var add = exports.add = function add(v1, v2) {
-    return v1.map(function (val, i) {
-        return v2[i] + val;
-    });
+  return v1.map(function (val, i) {
+    return v2[i] + val;
+  });
 };
 
 // For a given size, return an array of `size` with random values
@@ -46,15 +50,9 @@ var add = exports.add = function add(v1, v2) {
 //
 // https://github.com/mbostock/d3/wiki/Math
 var random = exports.random = function random(size) {
-    var mean = arguments.length <= 1 || arguments[1] === undefined ? 0.0 : arguments[1];
-    var deviation = arguments.length <= 2 || arguments[2] === undefined ? 1.0 : arguments[2];
-    return _fp2.default.map(function (i) {
-        return (0, _d3Random.randomNormal)(mean, deviation)();
-    }, _fp2.default.range(0, size));
-};
-
-var norm = exports.norm = function norm(v) {
-    return dist(v, v.map(function (s) {
-        return 0;
-    }));
+  var mean = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0.0;
+  var deviation = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1.0;
+  return _fp2.default.map(function (i) {
+    return (0, _d3Random.randomNormal)(mean, deviation)();
+  }, _fp2.default.range(0, size));
 };
